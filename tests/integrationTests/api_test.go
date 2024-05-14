dc9c4b3e7f9228cb71818d48d6d471a426a69ee0
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-05-14 16:21:30 -0700

    test(integration test): add integration tests

---
+++ tests/integrationTests/api_test.go
@@ -0,0 +1,37 @@
+package integrationTests
+
+import (
+	"bytes"
+	"github.com/gorilla/mux"
+	"httpServer/src/controller"
+	"httpServer/src/core"
+	"httpServer/src/middlewares/logging"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServerIntegration(t *testing.T) {
+	_ = &core.Api{Port: 8080}
+	router := mux.NewRouter()
+	router.Use(logging.Logging())
+	controller.InitControllers(router)
+	server := httptest.NewServer(router)
+	defer server.Close()
+	resp, err := http.Get(server.URL + "/health")
+	if err != nil {
+		t.Fatalf("failed to make GET request to server: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("unexpected status code: got %d want %d", resp.StatusCode, http.StatusOK)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	respBody := buf.String()
+	expectedRespBody := "This is the health endpoint\n"
+	if respBody != expectedRespBody {
+		t.Errorf("unexpected response body: got %q want %q", respBody, expectedRespBody)
+	}
+
+}
