60407a34179861b329d736877edfabf401039262
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-05-31 00:49:34 +0200

    test(integration): add more tests
    change initialisation of server
    test if route book is created with good routes

--- tests/integrationTests/api_test.go
+++ tests/integrationTests/api_test.go
@@ -14,15 +14,22 @@ import (
 	"testing"
 )
 
-func TestServerIntegration(t *testing.T) {
+func startServer() *httptest.Server {
 	var db database2.DatabaseInterface
 	var configuration models.Configuration
 	var dataModel []initialisation.DataModel
-	_ = &core.Api{Json: initialisation.JsonHandler{File: "config/config.test.json"}}
+	api := &core.Api{Json: initialisation.JsonHandler{File: "../../config/config.test.json"}}
+	api.Initialisation(&configuration, &dataModel)
+	db = &database2.MongoDB{Name: configuration.Db.Name, Url: configuration.Db.Url}
 	router := mux.NewRouter()
 	router.Use(logging.Logging())
 	controller.InitControllers(router, &configuration, &dataModel, db)
 	server := httptest.NewServer(router)
+	return server
+}
+
+func TestServerIntegrationHealth(t *testing.T) {
+	server := startServer()
 	defer server.Close()
 	resp, err := http.Get(server.URL + "/health")
 	if err != nil {
@@ -39,5 +46,24 @@ func TestServerIntegration(t *testing.T) {
 	if respBody != expectedRespBody {
 		t.Errorf("unexpected response body: got %q want %q", respBody, expectedRespBody)
 	}
+}
 
+func TestServerIntegrationBook(t *testing.T) {
+	server := startServer()
+	defer server.Close()
+	resp, err := http.Get(server.URL + "/book/read")
+	if err != nil {
+		t.Fatalf("failed to make GET request to server: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("unexpected status code: got %d want %d", resp.StatusCode, http.StatusOK)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	respBody := buf.String()
+	expectedRespBody := "404 page not found\n"
+	if respBody != expectedRespBody {
+		t.Errorf("unexpected response body: got %q want %q", respBody, expectedRespBody)
+	}
 }
